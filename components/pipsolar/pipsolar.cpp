ee8ca092e22ddd3a8771ff0f65ae9d57aa542fff
Faraz Ahmad <imfaraz101@gmail.com>
2024-06-01 02:11:26 +0500

    removed comma and space

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -477,7 +477,7 @@ void Pipsolar::loop() {
                &value_machine_type_, &value_topology_, &value_output_mode_,                                  // NOLINT
                &value_battery_redischarge_voltage_, &value_pv_ok_condition_for_parallel_,                    // NOLINT
                &value_pv_power_balance_, &value_battery_cutoff_voltage_, &value_dual_output_,
-               &value_battery_cutoff_voltage2_, );  // NOLINT
+               &value_battery_cutoff_voltage2_);  // NOLINT
         if (this->last_qpiri_) {
           ` this->last_qpiri_->publish_state(tmp);
         }
